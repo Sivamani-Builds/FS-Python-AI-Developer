697bf70ec591e99e16e0e92e6e391a7d4e3396a3
Manichilaka-9 <sivamanichilaka9@gmail.com>
2026-09-03 21:44:55 +0530

    Add Relational Operator program

---
+++ 01-python-basics/Operators/RelOpEx.py
@@ -0,0 +1,12 @@
+#Program for demonstrating the Functionaity of Relational Operators
+#RelOpEx1.py
+a=float(input("Enter Value of a:"))
+b=float(input("Enter Value of b:"))
+print("\tResults of Relational Operators")
+print("\t\t{} > {}= {}".format(a,b,a>b))
+print("\t\t{} < {}= {}".format(a,b,a<b))
+print("\t\t{} == {}= {}".format(a,b,a==b))
+print("\t\t{} != {}= {}".format(a,b,a!=b))
+print("\t\t{} >= {}= {}".format(a,b,a>=b))
+print("\t\t{} <= {}= {}".format(a,b,a<=b))
+#Here a>b, a<b a==b, a!=b a>=b  a<=b are called Relational Expressions.
