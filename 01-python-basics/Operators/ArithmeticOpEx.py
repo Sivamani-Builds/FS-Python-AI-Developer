4a5325f0cd2e2cce12d28060ca3458563644ac3d
Manichilaka-9 <sivamanichilaka9@gmail.com>
2026-09-03 20:15:27 +0530

    Add arithmetic operators program

---
+++ 01-python-basics/Operators/ArithmeticOpEx.py
@@ -0,0 +1,12 @@
+#program for Demonstrating the Functionality of Arithmetic Operationa
+#Arithematic Operations
+a=float(input("Enter value of a: "))
+b=float(input("Enter value of b: "))
+print("\t Results of Arithmetic Operations")
+print("\t\t Sum({},{})={}".format (a,b,a+b))
+print("\t\t Sub({},{})={}".format (a,b,a-b))
+print("\t\t Mul({},{})={}".format (a,b,a*b))
+print("\t\t Div({},{})={}".format (a,b,a/b))
+print("\t\t Mod Div({},{})={}".format (a,b,a//b))
+print("\t\t Floor Div({},{})={}".format (a,b,a%b))
+print("\t\t pow({},{})={}".format (a,b,a**b))
