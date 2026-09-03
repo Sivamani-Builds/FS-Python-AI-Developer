72bdc4dc155615a0626c8b712a6cfe639c1bfaf4
Manichilaka-9 <sivamanichilaka9@gmail.com>
2026-09-03 20:33:02 +0530

    Add assignment operators program

---
+++ 01-python-basics/Operators/AssignmentOpEx.py
@@ -0,0 +1,9 @@
+#program for Swapping of Any Two Values--Logic-1
+#AssignmentOpEx1.py
+a,b=input("Enter Value of a:"),input("Enter Value of b:")
+print("\tOriginal Val of a={}".format(a))
+print("\tOriginal Val of b={}".format(b))
+a,b=b,a # Swapping logic--
+print("\tSwapped Value of a={}".format(a))
+print("\tSwapped Value of b={}".format(b))
+
