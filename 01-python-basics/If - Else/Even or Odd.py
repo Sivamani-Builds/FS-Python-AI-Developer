485bc465f41ba2e3717332ecdd6d3918e280061d
Siva <sivamanichilaka9@gmail.com>
2026-09-06 00:14:01 +0530

    Delete 01-python-basics/If - Else/Even or Odd.py

--- 01-python-basics/If - Else/Even or Odd.py
+++
@@ -1,6 +0,0 @@
-# Write a python Program.If it is Even or Odd
-value=input("Enter Any Value:")
-if(value==value[::-1]):
-    print("\t{} is Palindrome ".format(value))
-elif (value != value[::-1]):
-    print("\t{} is not Palindrome".format(value))
